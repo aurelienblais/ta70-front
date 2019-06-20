e67f43b5ce6a93c1ae6a7a4ae0f2d2c47d21c5cc
paulantoine <paul.antoine.pr@gmail.com>
2019-06-20 14:15:47 +0200

    View for the crawl's list

--- src/app/crawls-view/crawls-view.page.ts
+++ src/app/crawls-view/crawls-view.page.ts
@@ -1,26 +1,107 @@
 import { Component, OnInit } from '@angular/core';
+import {first} from 'rxjs/operators';
+import {CrawlProviderService} from '../_services/crawl-provider.service';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-crawls-view',
   templateUrl: './crawls-view.page.html',
   styleUrls: ['./crawls-view.page.scss'],
 })
+
 export class CrawlsViewPage implements OnInit {
 
-  constructor() { }
+  crawlList: any[] = [
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      { titre: 'test', test: 'test'}, { titre: 'test2', test: 'test'},
+      ];
+
+  constructor(private crawlProviderService: CrawlProviderService, public toastController: ToastController) { }
 
   ngOnInit() {
-    /*showCrawls()
-    {
-      this.crawlProviderService.getCrawls(1)
+  }
+
+  showCrawls() {
+      this.crawlProviderService.getPublicCrawls()
           .pipe(first())
           .subscribe(
               data => {
-                for ( const  poi of data) { this.addMarkerOnMap(poi); }
+                for ( const  crawl of data) { this.addToList(crawl); }
               },
-              error => { console.log('error'); }
+              error => { console.log(error); }
           );
-    }*/
+    }
+
+  addToList( crawl) {
+    this.crawlList.push(crawl);
   }
+    vision(item) {
+        this.visionToast();
+    }
+
+    duplicate( crawl) {
+        this.duplicateToast();
+    }
+
+    delete( crawl){
+      this.deleteToast();
+    }
+    async visionToast() {
+        this.showToast('Barathon passé en privé/publique');
+    }
+
+    async duplicateToast() {
+        this.showToast('Barathon dupliqué');
+    }
+
+
+    async deleteToast() {
+        this.showToast('Barathon supprimé' );
+    }
+
+    async showToast( message ) {
+
+        const toast = await this.toastController.create({
+                message,
+                duration: 4000,
+                position: 'bottom',
+                buttons: [
+                    {
+                        text: 'Done',
+                        role: 'cancel',
+                        handler: () => {
+                            console.log('Cancel clicked');
+                        }
+                    }
+                ]
+            }
+        );
+        toast.present();
+    }
 
 }
+
+
+
+
